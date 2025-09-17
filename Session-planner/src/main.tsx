101b814bc56fc8eb681e20634912dd18f5b73cd7
Uzimonkey123 <normanbabiak@gmail.com>
2025-09-17 17:14:35 +0300

    feat: Material tailwind + react router setup

--- Session-planner/src/main.tsx
+++ Session-planner/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@material-tailwind/react/context/theme'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+	<ThemeProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ Session-planner/src/material-tailwind.d.ts
@@ -0,0 +1,7 @@
+declare module '@material-tailwind/react' {
+    export const Card: any;
+    export const CardBody: any;
+    export const Typography: any;
+    export const Button: any;
+    export const Navbar: any;
+}
